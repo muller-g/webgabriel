6fe6970432ce066865c975588a470be2354e6f1f
mullerg <gabriel_muller98@hotmail.com>
2025-03-06 09:55:47 -0300

    fix: ajuste faltante erro no hook de fetch

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -4,14 +4,11 @@ import styles from "./page.module.css";
 import { authOptions } from "@/utils/authOptions";
 
 export default async function Portfolio() {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_ROUTE + '/developer');
     const session = await getServerSession(authOptions);
     
-    const data = await response.json(); 
-
     return (
       <main className={styles.main}>
-        <PortfolioEdit data={data} session={session}/>
+        <PortfolioEdit session={session}/>
       </main>
     );
 }
